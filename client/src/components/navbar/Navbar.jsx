52befc9df2c8fef55544b28c1934a407216bfebc
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-07-26 18:10:28 +0530

    first commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.scss";
+
+function Navbar() {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const { pathname } = useLocation();
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  // const currentUser = null
+
+  const currentUser = {
+    id: 1,
+    username: "Abhijit",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link className="link" to="/">
+            <span className="text">FreeWork</span>
+          </Link>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Liverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {currentUser ? (
+            <div className="user" onClick={()=>setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && <div className="options">
+                {currentUser.isSeller && (
+                  <>
+                    <Link className="link" to="/gigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
+                  </>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <Link className="link" to="/">
+                  Logout
+                </Link>
+              </div>}
+            </div>
+          ) : (
+            <>
+              <span>Sign in</span>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/gigs">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/gigs">
+              Lifestyle
+            </Link>
+          </div>
+          <hr />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
