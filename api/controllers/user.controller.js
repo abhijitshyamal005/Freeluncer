607034cf9a84ca20b56e74de6a0b9fe49e413a4c
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-07-26 21:15:04 +0530

    third commit

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const fn = (req,res)=>{
+    res.send("user deleted");
+}
+
+export default fn;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import userRoute from './routes/user.route.js';
 
 const app = express();
+dotenv.config();
+mongoose.set('strictQuery', true);
+
+const connect = async()=>{
+try {
+    await mongoose.connect(process.env.DATABASE);
+    console.log("connected to database");
+    
+} catch (error) {
+    console.log(error);
+    
+}
+}
+
+app.use("/api/user",userRoute)
 
 app.listen(8800,()=>{
+    connect();
     console.log('Server is running on port 8800');
 })
